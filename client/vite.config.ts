bad49fb6d0b785737c4fee8bfdf7ea6c35a2e6c0
sa3akash <sa3akash@gmail.com>
2024-07-25 22:44:42 +0600

    update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,11 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    // host: 'localhost',
+    watch:{
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
     port: 3000
   }
 })
