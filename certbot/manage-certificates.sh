f3aa72a912bc0c5fa050d3f439c33566559eeeb5
sa3akash <sa3akash@gmail.com>
2024-07-27 21:53:59 +0600

    build

--- certbot/manage-certificates.sh
+++ certbot/manage-certificates.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
+#!/bin/sh
+# This script will check if the certificates exist and renew or obtain them accordingly
 
-# Start Nginx to serve the challenge
-nginx
+# Obtain new certificates or renew existing ones
+certbot certonly --webroot --webroot-path=/var/www/certbot --email $CERTBOT_EMAIL --agree-tos --no-eff-email --domains $CERTBOT_DOMAINS
 
-# Obtain or renew certificates
-certbot --nginx --non-interactive --agree-tos --email ${EMAIL} --domains ${DOMAINS}
-
-# Reload Nginx to apply new certificates
+# Reload Nginx to apply the new certificates
 nginx -s reload
