5648e0b6eb8ff8b97e1a90baa9c454d7525b7ba9
sa3akash <sa3akash@gmail.com>
2024-07-27 20:51:05 +0600

    build all

--- certbot/manage-certificates.sh
+++ certbot/manage-certificates.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 # This script will check if the certificates exist and renew or obtain them accordingly
 
+# Start the minimal Nginx server for handling ACME challenges
+nginx -c /etc/nginx/nginx.conf
+
 # Obtain new certificates or renew existing ones
 certbot certonly --webroot --webroot-path=/var/www/certbot --email "$CERTBOT_EMAIL" --agree-tos --no-eff-email --domains $CERTBOT_DOMAINS
 
-# Reload Nginx to apply the new certificates
-nginx -s reload
+# Stop the Nginx server
+nginx -s stop
+
+# Reload Nginx in the main container to apply the new certificates
+# docker-compose exec nginx nginx -s reload
